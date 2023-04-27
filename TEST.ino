521ce3822e038c1b65196f781e46734198382d0f
Kondepu Nagarjuna <109850570+Nagarjun444@users.noreply.github.com>
2023-04-27 21:58:56 +0530

    Add files via upload

---
+++ TEST.ino
@@ -0,0 +1,47 @@
+//#define ledpin1 2
+//#define ledpin2 16
+//
+void setup()
+{
+//  pinMode(ledpin1,OUTPUT);
+ // pinMode(ledpin2,OUTPUT); 
+  }
+
+  void loop()
+  {
+    //digitalWrite(ledpin1,LOW);
+   // digitalWrite(ledpin2,HIGH);
+   // delay(1000);
+    
+   // digitalWrite(ledpin1,HIGH);
+   // digitalWrite(ledpin2,LOW);
+  //  delay(1000);
+  //  printf("display");
+    
+    }
+
+
+//--------------------WIFI-------------------------------
+
+
+//#include<ESP8266WiFi.h>
+//void setup()
+//{
+//  Serial.begin(115200); //baud rate
+//  WiFi.mode(WIFI_STA); //---node as station
+//  WiFi.begin("Chinna-2.4G","Chinna@987");
+ // Serial.print("connecting to wifi");
+//  while(WiFi.status()!=WL_CONNECTED)
+//  {
+//    Serial.print('.');
+ //   delay(200);
+ // }
+ // Serial.print("IP Address:");
+ // Serial.println(WiFi.localIP());
+ // Serial.print("MacAddress:");
+ // Serial.println(WiFi.macAddress());
+  
+ // }
+//void loop()
+//{
+//  }
